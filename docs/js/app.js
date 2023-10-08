b2576caec177eb10cdeff20b688f91060b870fc9
Alvaro Gonzalez <varogonz95@gmail.com>
2023-10-08 17:54:56 -0600

    info planetas y rotacion

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,44 +1,28 @@
-window.addEventListener('load', initScene)
 
-const meteors = [
-    { x: 0, y: 0, z: -30 },
-    { x: 0, y: 0, z: 30 },
-    { x: 30, y: 0, z: 0 },
-    { x: -30, y: 0, z: 0 },
-    { x: 20, y: 0, z: 20 },
-    { x: 20, y: 0, z: -20 },
-    { x: -20, y: 0, z: -20 },
-    { x: -20, y: 0, z: 20 }
-]
-
-let meteor, score = 0
-
-function initScene() {
-
-    let orbits = document.querySelectorAll('.orbit')
-
-    orbits.forEach(orbit => {
-
-        meteors.forEach(pos => {
-
-            meteor = document.createElement('a-entity')
-            meteor.setAttribute('geometry', { primitive: 'sphere', radius: Math.random() * 3 + 0.5 })
-            meteor.setAttribute('material', { shader: 'flat', src: '#meteor' })
-            meteor.setAttribute('class', 'meteor')
-            meteor.object3D.position.set(pos.x, pos.y, pos.z)
-
-            meteor.setAttribute('shootable', '')
-
-            orbit.appendChild(meteor)
-        })
-    })
-}
-
-AFRAME.registerComponent('shootable', {
+AFRAME.registerComponent('planet', {
+    schema: { info: { type: 'string' } },
     init: function () {
-        this.el.addEventListener('click', () => {
-            this.el.parentNode.removeChild(this.el)
-            document.querySelector('[text]').setAttribute('value', `${++score} meteoritos cazados`)
-        })
+        const $this = this
+        const planetElm = this.el
+        const planetInfo = document.querySelector('#planet-info')
+        const camera = document.querySelector('#camera')
+
+        planetElm.addEventListener('mouseenter', (evt) => {
+            document.querySelector('#orbit').emit('pauseOrbit', {}, false);
+
+            const infoText = $this.data.info
+            planetInfo.setAttribute('value', infoText)
+
+            const { x, y, z } = camera.object3D.position
+            planetInfo.object3D.position.set(x - 2.5, y, z)
+            planetInfo.object3D.rotation.set(0, 90, 0)
+            planetInfo.setAttribute('visible', true)
+        });
+
+        planetElm.addEventListener('mouseleave', (evt) => {
+            document.querySelector('#orbit').emit('resumeOrbit', {}, false);
+            planetInfo.setAttribute('visible', false)
+        });
     }
-})
+});
+
